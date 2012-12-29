14688512cec09023cbd1ac1b81964075dfb6dc86
Mike Abner <mike.abner@gmail.com>
2012-12-29 11:39:18 -0800

    'pincher' overlays to highlight an individual cell when it is clicked.

--- app/list_view_cell.rb
+++ app/list_view_cell.rb
@@ -10,49 +10,27 @@ class ListViewCell < UITableViewCell
 
     @color_bar = UIView.alloc.init
     @color_bar.frame = [[0, 0], [10, self.frame.size.height]]
-    @color_bar.backgroundColor = [UIColor.greenColor, UIColor.blueColor].sample
+    @color_bar.backgroundColor = [UIColor.greenColor, UIColor.blueColor, UIColor.whiteColor].sample
 
     @item_name = UILabel.alloc.init
     @item_name.frame = [[20, 11], [240, 22]]
-    @item_name.setFont(UIFont.fontWithName("Futura-Medium", size:14))
+    @item_name.setFont(UIFont.fontWithName("Futura-Medium", size:16))
     @item_name.text = item[:title]
-    @item_name.backgroundColor = UIColor.purpleColor
+    #@item_name.backgroundColor = UIColor.purpleColor
 
     @trigger = UIButton.buttonWithType(UIButtonTypeCustom)
     @trigger.frame = [[280, 7], [30, 30]]
     @trigger.setImage(UIImage.imageNamed("city_find_me"), forState:UIControlStateNormal)
     @trigger.setImage(UIImage.imageNamed("city_find_me_down"), forState:UIControlStateHighlighted)
-
-    # @trigger.backgroundColor = UIColor.blackColor
-    # @long_press = UILongPressGestureRecognizer.alloc.initWithTarget(self, action:"handleLongPressGesture")
-    # @long_press.minimumPressDuration = 0.35
-    # @trigger.addGestureRecognizer(@long_press)
-
+    @trigger.addTarget(self, action:"didPressActionTrigger", forControlEvents:UIControlEventTouchUpInside)
 
     self.contentView.addSubview(@color_bar)
     self.contentView.addSubview(@item_name)
     self.contentView.addSubview(@trigger)
   end
 
-  def handleLongPressGesture
-    if (@long_press.state == UIGestureRecognizerStateBegan)
-      #@parent.didActivateMenuForCell(self)
-      @expandingSelect = KLExpandingSelect.alloc.initWithDelegate(self, dataSource:self)
-
-      self.contentView.setExpandingSelect(@expandingSelect)
-      self.contentView.addSubview(@expandingSelect)
-    end
-  end
-
-  # KLEXPANDINGSELECT DELEGATE
-  def expandingSelector(expandingSelect, numberOfRowsInSection:section)
-    puts "num petals"
-    3
-  end
-
-  def expandingSelector(expandingSelect, itemForRowAtIndexPath:indexPath)
-    puts "item for petal"
-    petal = KLExpandingPetal.alloc.initWithImage(UIImage.imageNamed:"gear");
+  def didPressActionTrigger
+    @parent.didActivateMenuForCell(self)
   end
 
 end
--- app/list_view_controller.rb
+++ app/list_view_controller.rb
@@ -48,19 +48,49 @@ class ListViewController < UIViewController
   end
 
   def didActivateMenuForCell(cell)
-    @index_path = @table_view.indexPathForCell(cell)
-    # show_menu(cell.frame.origin.y)
-    puts cell.frame.origin.y
-    puts @table_view.contentOffset.y
-    @expandingSelect = KLExpandingSelect.alloc.initWithDelegate(self, dataSource:self)
+    slider_height           = UIScreen.mainScreen.applicationFrame.size.height
+    top_slider_origin_y     = -slider_height
+    bottom_slider_origin_y  = slider_height
 
-    self.view.setExpandingSelect(@expandingSelect)
-    self.view.addSubview(@expandingSelect)
+    if @sliders_shown
+      UIView.animateWithDuration(0.35, animations:lambda { 
+        @top_slider.frame     = [[0, top_slider_origin_y],    [320, slider_height]]
+        @bottom_slider.frame  = [[0, bottom_slider_origin_y], [320, slider_height]]
+      })
+    else
+      if !@top_slider
+        @top_slider         = UIView.alloc.init
+        @top_slider.frame   = [[0, top_slider_origin_y], [320, slider_height]]
+        @top_slider.opaque  = false
+        @top_slider.alpha   = 0.90
+        @top_slider.backgroundColor = UIColor.blackColor
+
+        @bottom_slider        = UIView.alloc.init
+        @bottom_slider.frame  = [[0, bottom_slider_origin_y], [320, slider_height]]
+        @bottom_slider.opaque = false
+        @bottom_slider.alpha  = 0.90
+        @bottom_slider.backgroundColor = UIColor.blackColor
+
+        self.view.addSubview(@top_slider)
+        self.view.addSubview(@bottom_slider)
+      end
+
+      top_slider_offset       = @table_view.frame.origin.y + cell.frame.origin.y - @table_view.contentOffset.y
+      top_slider_origin_y_new = top_slider_origin_y + top_slider_offset
+
+      bottom_slider_offset        = @table_view.frame.origin.y + cell.frame.origin.y + cell.frame.size.height - @table_view.contentOffset.y
+
+      UIView.animateWithDuration(0.35, animations:lambda { 
+        @top_slider.frame     = [[0, top_slider_origin_y_new], [320, slider_height]] 
+        @bottom_slider.frame  = [[0, bottom_slider_offset], [320, slider_height]]
+      })
+    end
+    @sliders_shown = !@sliders_shown
   end
 
   def didPressMenuCancelBtn
     @menu_bg.hidden = true
-    @menu.hidden = true
+    @menu.hidden    = true
   end
 
 private
@@ -72,25 +102,25 @@ private
   end
 
   def draw_header
-    @header = UIView.alloc.init
-    @header.frame = [[0, 0], [320, 44]]
-    @header_bg = UIImageView.alloc.initWithImage(UIImage.imageNamed("header-bg"))
-    @header_bg.frame = @header.frame
+    @header           = UIView.alloc.init
+    @header.frame     = [[0, 0], [320, 44]]
+    @header_bg        = UIImageView.alloc.initWithImage(UIImage.imageNamed("header-bg"))
+    @header_bg.frame  = @header.frame
     @header.addSubview(@header_bg)
 
-    @title = UILabel.alloc.init
-    @title.frame = [[110, 6], [100, 35]]
-    @title.font = UIFont.fontWithName("Futura-CondensedExtraBold", size:20)
-    @title.text = "Honeydew"
+    @title        = UILabel.alloc.init
+    @title.frame  = [[110, 6], [100, 35]]
+    @title.font   = UIFont.fontWithName("Futura-CondensedExtraBold", size:20)
+    @title.text   = "Honeydew"
     @title.backgroundColor = UIColor.clearColor
 
-    @add_btn = UIButton.buttonWithType(UIButtonTypeCustom)
+    @add_btn       = UIButton.buttonWithType(UIButtonTypeCustom)
     @add_btn.frame = [[280, 8], [30, 30]]
     @add_btn.setImage(UIImage.imageNamed("simple_plus"), forState:UIControlStateNormal)
     @add_btn.setImage(UIImage.imageNamed("simple_plus_down"), forState:UIControlStateHighlighted)
     @add_btn.addTarget(self, action:"didPressAddButton", forControlEvents:UIControlEventTouchUpInside)
 
-    @settings_btn = UIButton.buttonWithType(UIButtonTypeCustom)
+    @settings_btn       = UIButton.buttonWithType(UIButtonTypeCustom)
     @settings_btn.frame = [[10, 8], [30, 30]]
     @settings_btn.setImage(UIImage.imageNamed("simple_minus"), forState:UIControlStateNormal)
     @settings_btn.setImage(UIImage.imageNamed("simple_minus_down"), forState:UIControlStateHighlighted)
@@ -99,43 +129,46 @@ private
     @header.addSubview(@title)
     @header.addSubview(@add_btn)
     @header.addSubview(@settings_btn)
+
     self.view.addSubview(@header)
   end
 
   def draw_table
-    @table_view = UITableView.alloc.init
+    @table_view       = UITableView.alloc.init
     @table_view.frame = [[0, @header.frame.size.height], [320, UIScreen.mainScreen.applicationFrame.size.height-50-@header.frame.size.height]]
 
     @table_view.delegate    = self
     @table_view.dataSource  = self
 
     @table_view.allowsSelection = false
-    @table_view.separatorStyle = UITableViewCellSeparatorStyleNone
+    @table_view.separatorStyle  = UITableViewCellSeparatorStyleNone
+
     self.view.addSubview(@table_view)
   end
 
   def draw_footer
-    @footer = UIView.alloc.init
+    @footer       = UIView.alloc.init
     @footer.frame = [[0, UIScreen.mainScreen.applicationFrame.size.height-50], [320, 50]]
     @footer.backgroundColor = UIColor.blueColor
+
     self.view.addSubview(@footer)
   end
 
   def show_menu(y_origin)
-    @menu_bg = UIView.alloc.init unless @menu_bg
+    @menu_bg        = UIView.alloc.init unless @menu_bg
     @menu_bg.hidden = false
-    @menu_bg.frame = self.view.frame
+    @menu_bg.frame  = self.view.frame
+    @menu_bg.alpha  = 0.85
     @menu_bg.backgroundColor = UIColor.lightGrayColor
-    @menu_bg.alpha = 0.85
 
-    start_x = 290
-    x_origin = 90
-    start_y = @table_view.frame.origin.y
-    y_origin = @table_view.frame.origin.y + y_origin - @table_view.contentOffset.y
+    start_x   = 290
+    x_origin  = 90
+    start_y   = @table_view.frame.origin.y
+    y_origin  = @table_view.frame.origin.y + y_origin - @table_view.contentOffset.y
     
-    @menu = UIView.alloc.init unless @menu
-    @menu.hidden = false
-    @menu.frame = [[start_x, start_y], [0, 0]]
+    @menu         = UIView.alloc.init unless @menu
+    @menu.hidden  = false
+    @menu.frame   = [[start_x, start_y], [0, 0]]
 
     UIView.animateWithDuration(0.35, animations:lambda { @menu.frame = [[x_origin, y_origin], [200, 200]] })
 
